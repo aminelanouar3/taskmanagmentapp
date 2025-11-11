b068b620412805c3b5c4d261720254861b412158
aminelanouar <aminelanouar3@gmail.com>
2025-11-11 10:24:24 +0100

    ADD PIPE AND SERVICES

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,29 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Task } from "./task/task";
+import { Taskservice } from './taskservice';
+import { CurrencyPipe, DatePipe, DecimalPipe, LowerCasePipe, PercentPipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Task],
+  imports: [RouterOutlet, Task,UpperCasePipe,LowerCasePipe,TitleCasePipe,DatePipe,DecimalPipe,CurrencyPipe,PercentPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('todolist');
-  tasks = [
-    { id:1,title: 'Learn Angular', done: false },
-    { id:2,title: 'Build ToDoList App', done: false },
-    { id:3,title: 'Celebrate!', done: true }
-  ];
+  tasks: any[] =[]; 
+  constructor(private taskService:Taskservice){}
+  ngOnInit(){
+    this.tasks=this.taskService.getTasks();
+  }
   addtask(){
     this.tasks.push({id:4,title:'new task',done:false})
   }
   deletetask(ind:number){
     this.tasks.splice(ind,1);
   }
+  today = new Date(); 
+  price = 1234.56;
+  percentage = 0.259;
 }
